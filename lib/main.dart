c3e8d4c3782d3b589dc31cb33be8ed432f8eeda1
Huy Nguyen <huyn120198@gmail.com>
2020-09-21 17:06:42 -0500

    Inital Flutter Codes

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//We are currently adding widgets and editing properties of those widgets
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("I Am Rich")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
